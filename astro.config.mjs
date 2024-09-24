7a941a15d56410c7acc54381458407704c4b4966
Patricio Saldía <psaldias@mandrildigital.cl>
2024-09-24 15:31:09 -0300

    Estrucutra de landing listo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  base: "/alfred",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
 });
