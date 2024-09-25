b38c31205ad6add9017bbd8706f12a3f7f4d9187
Patricio Saldía <psaldias@mandrildigital.cl>
2024-09-25 18:49:37 -0300

    Correción github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   // base: "/alfred",
+  site: "https://creatividad-e-inteligencia.github.io",
+  base: "alfred-landing",
   integrations: [
     tailwind({
       applyBaseStyles: false,
