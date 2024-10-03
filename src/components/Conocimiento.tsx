95c6f7261388d36482b5bdd8bf0192fe2a9f4b16
Patricio Saldía <psaldias@mandrildigital.cl>
2024-10-03 12:20:45 -0300

    Cambios texto e imagen de conocimiento

--- src/components/Conocimiento.tsx
+++ src/components/Conocimiento.tsx
@@ -5,25 +5,23 @@ export default function Contenido() {
   return (
     <div className="prose sm:text-xl max-w-5xl mx-auto py-8 px-4">
       <p>
-        Finalmente, toda la información recopilada y registrada durante la
-        reunión{" "}
-        <strong>
-          se almacena de manera segura en un repositorio centralizado denominado
-          "Conocimiento"
-        </strong>
-        . Este repositorio es accesible únicamente por miembros de la
-        organización,{" "}
-        <strong>
-          quienes pueden realizar consultas utilizando lenguaje natural a través
-          de correo electrónico o WhatsApp
-        </strong>
-        , permitiendo así un fácil acceso y gestión de la información en
-        cualquier momento.
+        Toda la información recopilada y registrada durante las reuniones se
+        almacenan de forma segura en un repositorio centralizado llamado{" "}
+        <strong>Conocimiento</strong>, el que se irá{" "}
+        <strong>alimentando y entrenando en el tiempo</strong> para una mejor
+        gestión entre CI y nuestros clientes.
+      </p>
+      <p>
+        Este repositorio es accesible{" "}
+        <strong>exclusivamente para usuarios habilitados</strong>, quienes
+        pueden realizar consultas mediante lenguaje{" "}
+        <strong>natural a través de WhatsApp</strong>, lo que facilita el acceso
+        y la gestión de la información en cualquier momento.
       </p>
       <div className="w-10/12 mx-auto">
         <ModalImage
-          small="/como-invitar/conocimiento.jpg?v=001"
-          large="/como-invitar/conocimiento.jpg?v=001"
+          small="/como-invitar/conocimiento.v2.jpg?v=001"
+          large="/como-invitar/conocimiento.v2.jpg?v=001"
           alt="Conocimiento"
           className="mx-auto"
         />
